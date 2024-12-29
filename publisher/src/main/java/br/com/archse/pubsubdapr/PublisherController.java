940961269208603d3b9bf0575ddb2c602f845ef0
Manoel Valerio da Silveira Neto <45215788+manoelvsneto@users.noreply.github.com>
2024-12-29 18:54:08 -0300

    Update PublisherController.java

--- publisher/src/main/java/br/com/archse/pubsubdapr/PublisherController.java
+++ publisher/src/main/java/br/com/archse/pubsubdapr/PublisherController.java
@@ -1,22 +1,15 @@
 package br.com.archse.pubsubdapr;
 
-import io.dapr.client.DaprClient;
-import io.dapr.client.DaprClientBuilder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class PublisherController {
 
-    private static final String PUBSUB_NAME = "redis-pubsub";
-    private static final String TOPIC_NAME = "sample-topic";
 
     @GetMapping("/publish")
-    public String publishMessage() throws Exception {
-        try (DaprClient client = new DaprClientBuilder().build()) {
-            String message = "Hello, Dapr!";
-            client.publishEvent(PUBSUB_NAME, TOPIC_NAME, message).block();
-            return "Message published: " + message;
-        }
+    public String publishMessage()  {
+            return "Message published" ;
+        
     }
 }
