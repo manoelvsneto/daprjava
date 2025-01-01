7dc73b0ca815b55566bed31cdf7a2818292823cb
Manoel Valerio da Silveira Neto <manoelneto@hotmail.com>
2025-01-01 19:14:13 -0300

    aa
    aa

--- publisher/src/main/java/br/com/archse/pubsubdapr/PublisherController.java
+++ publisher/src/main/java/br/com/archse/pubsubdapr/PublisherController.java
@@ -1,9 +1,11 @@
 package br.com.archse.pubsubdapr;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/api/publisher")
 public class PublisherController {
 
 
